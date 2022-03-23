ec17d6fc2b28e1f9a1ad1790d8646dc45cba67fa
Robert <robert1krizan2@gmail.com>
2022-03-23 23:17:06 +0100

    naformatovanie kodu

--- JS/animations.js
+++ JS/animations.js
@@ -1,26 +1,26 @@
 //bounce when you add new letter
 const animateTileBounce = (tile) => {
-    tile.classList.add('is-filled', 'animate__animated', 'animate__bounceIn');
-}
+  tile.classList.add("is-filled", "animate__animated", "animate__bounceIn");
+};
 
 //shake whole row, when you submit a not-existant word
 const animeRowShake = (row) => {
-    row.classList.remove('animate__shakeX');
-    setTimeout(()=>{
-        row.classList.add('animate__animated', 'animate__shakeX'); 
-    },0)
-}
+  row.classList.remove("animate__shakeX");
+  setTimeout(() => {
+    row.classList.add("animate__animated", "animate__shakeX");
+  }, 0);
+};
 
 //rotate tile, when you submit an existing word
 const animateTileReveal = (row) => {
-    row.querySelectorAll('.tile').forEach((tile,index) => {
-        tile.classList.remove('animate__bounceIn', 'animate__flipInY');
-        setTimeout(()=>{
-            tile.style.visibility = 'visible';
-            tile.classList.add('animate__flipInY', `animate__delay-${index}s`);
-        },0);
+  row.querySelectorAll(".tile").forEach((tile, index) => {
+    tile.classList.remove("animate__bounceIn", "animate__flipInY");
+    setTimeout(() => {
+      tile.style.visibility = "visible";
+      tile.classList.add("animate__flipInY", `animate__delay-${index}s`);
+    }, 0);
 
-        /*
+    /*
         if(index==maxWordLenght-1){
             tile.addEventListener('animationend', () => {
                 judgeResult();
@@ -28,72 +28,79 @@ const animateTileReveal = (row) => {
             });
         }
         */
-    });
-}
+  });
+};
 
 //dance when you win
 const animateTileDance = (row) => {
-    row.querySelectorAll('.tile').forEach((tile,index) => {
-        tile.innerHTML = solution.charAt(index);
+  row.querySelectorAll(".tile").forEach((tile, index) => {
+    tile.innerHTML = solution.charAt(index);
 
-        tile.classList.remove('animate__bounceIn', 'animate__flipInY', 'animate__bounce');
+    tile.classList.remove(
+      "animate__bounceIn",
+      "animate__flipInY",
+      "animate__bounce"
+    );
 
-        setTimeout(()=>{
-            tile.classList.add('animate__bounce', `animate__delay-${index}s`);
-        },0);
-    });
-}
+    setTimeout(() => {
+      tile.classList.add("animate__bounce", `animate__delay-${index}s`);
+    }, 0);
+  });
+};
 
 //game over
 const youVeryMuchLose = () => {
-    let board = document.querySelector('.board');
-    board.classList.add('animate__animated', 'animate__hinge');
-    setTimeout(()=>{
-        let correctWord = document.querySelector(".correctAnswer");
-        correctWord.textContent= solution.toUpperCase();
-        animateWordBackIn(correctWord);
-    },2000)
-}
+  let board = document.querySelector(".board");
+  board.classList.add("animate__animated", "animate__hinge");
+  setTimeout(() => {
+    let correctWord = document.querySelector(".correctAnswer");
+    correctWord.textContent = solution.toUpperCase();
+    animateWordBackIn(correctWord);
+  }, 2000);
+};
 
-//correct answer 
+//correct answer
 const animateWordBackIn = (word) => {
-    word.classList.add('animate__animated', 'animate__backInUp');
-}
+  word.classList.add("animate__animated", "animate__backInUp");
+};
 
 //highligh letter
 const highlightLetters = (row) => {
-    let pressentLetters =[];
+  let pressentLetters = [];
 
-    row.querySelectorAll('.tile').forEach((tile,index) => {
-        tile.style.visibility = 'hidden';
-        
-        let letter = noAccents(word.charAt(index));
-        let colorClass = 'wrong';
+  row.querySelectorAll(".tile").forEach((tile, index) => {
+    tile.style.visibility = "hidden";
 
-        //ak psimena su aj spravne aj obsahuje zobraz len spravne
-        //zobraz len jedno zlte pismeno
-        if(noAccentSolution.includes(letter)){
-            if(!lettersInRow.correct.includes(letter) && !pressentLetters.includes(letter)){
-                colorClass='present';
-                pressentLetters.push(letter);
-            }
-        }
+    let letter = noAccents(word.charAt(index));
+    let colorClass = "wrong";
 
-        if(noAccentSolution.charAt(index)==letter){
-            colorClass = 'correct';
-        }
+    //ak psimena su aj spravne aj obsahuje zobraz len spravne
+    //zobraz len jedno zlte pismeno
+    if (noAccentSolution.includes(letter)) {
+      if (
+        !lettersInRow.correct.includes(letter) &&
+        !pressentLetters.includes(letter)
+      ) {
+        colorClass = "present";
+        pressentLetters.push(letter);
+      }
+    }
 
-        tile.classList.add(colorClass);
-    });
+    if (noAccentSolution.charAt(index) == letter) {
+      colorClass = "correct";
+    }
 
-    // keyboard row in footer
-	document.querySelectorAll('.keyboard .tile').forEach(tile => {
-		let colorClass = ''
+    tile.classList.add(colorClass);
+  });
 
-		if (lettersInRow.wrong.includes(tile.id)) colorClass = 'wrong'
-		if (lettersInRow.present.includes(tile.id)) colorClass = 'present'
-		if (lettersInRow.correct.includes(tile.id)) colorClass = 'correct'
+  // keyboard row in footer
+  document.querySelectorAll(".keyboard .tile").forEach((tile) => {
+    let colorClass = "";
 
-		if (colorClass) tile.classList.add(colorClass)
-	})
-}
+    if (lettersInRow.wrong.includes(tile.id)) colorClass = "wrong";
+    if (lettersInRow.present.includes(tile.id)) colorClass = "present";
+    if (lettersInRow.correct.includes(tile.id)) colorClass = "correct";
+
+    if (colorClass) tile.classList.add(colorClass);
+  });
+};
--- JS/app.js
+++ JS/app.js
@@ -2,160 +2,152 @@
 const maxWordLenght = 5;
 const countOfTries = 6;
 
-let word ='';
+let word = "";
 let tries = 1;
 
-let solution = allWords[allWords.length * Math.random() | 0].toLowerCase();
+let solution = allWords[(allWords.length * Math.random()) | 0].toLowerCase();
 let noAccentSolution = noAccents(solution);
-let noAccentWords = allWords.map(x => noAccents(x));
+let noAccentWords = allWords.map((x) => noAccents(x));
 
 let lettersInRow = {
-	correct: [],
-	present: [],
-	wrong: []
-}
+  correct: [],
+  present: [],
+  wrong: [],
+};
 
 //keyboard
-document.addEventListener('keydown', (event)=>{
-    if(event.key == 'Enter'){
-        submitWord();
-    }
-    else if(event.key == 'Backspace'){
-        removeLetter();
-    }
-    else{
-        addLetter(event.key);
-    }
+document.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+    submitWord();
+  } else if (event.key == "Backspace") {
+    removeLetter();
+  } else {
+    addLetter(event.key);
+  }
 });
 
 //submit
 const submitWord = () => {
-    if( word.length != maxWordLenght) return;
+  if (word.length != maxWordLenght) return;
 
-    
-    //is this a real world?
-    if(!noAccentWords.includes(noAccents(word))){
-        animeRowShake(currentRow());
-        return ;
-    }
+  //is this a real world?
+  if (!noAccentWords.includes(noAccents(word))) {
+    animeRowShake(currentRow());
+    return;
+  }
 
-    findLettersInRow();
-    //console.dir(lettersInRow);
+  findLettersInRow();
+  //console.dir(lettersInRow);
 
-    highlightLetters(currentRow());
-    animateTileReveal(currentRow());
-    setTimeout(() => {
-        judgeResult();
-    },1500)
-}
+  highlightLetters(currentRow());
+  animateTileReveal(currentRow());
+  setTimeout(() => {
+    judgeResult();
+  }, 1500);
+};
 
 //add letters
 const addLetter = (character) => {
-    if( word.length >= maxWordLenght) return;
+  if (word.length >= maxWordLenght) return;
 
-    //allow only latters
-    if(/^\p{L}$/u.test(character)){
-        word = word + character;
-        word = word.toLowerCase();
-
-        let tile = currentTile();
-        tile.innerHTML = character;
-        animateTileBounce(tile);
-        
-    }
-
-    //console.log(word);
-}
-
-const removeLetter = () => {
-    if( word.length <= 0) return;
+  //allow only latters
+  if (/^\p{L}$/u.test(character)) {
+    word = word + character;
+    word = word.toLowerCase();
 
     let tile = currentTile();
-    tile.innerHTML = '';
-    tile.className = 'tile';
+    tile.innerHTML = character;
+    animateTileBounce(tile);
+  }
 
-    word = word.slice(0 , -1);
-    //console.log(word);
-}
+  //console.log(word);
+};
+
+const removeLetter = () => {
+  if (word.length <= 0) return;
+
+  let tile = currentTile();
+  tile.innerHTML = "";
+  tile.className = "tile";
+
+  word = word.slice(0, -1);
+  //console.log(word);
+};
 
 //tile to update
 const currentTile = () => {
-    return currentRow().querySelector(':nth-child('+word.length+')');
-}
+  return currentRow().querySelector(":nth-child(" + word.length + ")");
+};
 
 //curentRow
 const currentRow = () => {
-    return document.querySelector('.row:nth-child('+tries+')');
-}
+  return document.querySelector(".row:nth-child(" + tries + ")");
+};
 
 const judgeResult = () => {
-    if(noAccents(word) == noAccentSolution){
-        animateTileDance(currentRow());
-    }
-    else if(tries >= countOfTries){
-        youVeryMuchLose();
-    }
-    else{
-        word='';
-        tries++;
-    }
-}
+  if (noAccents(word) == noAccentSolution) {
+    animateTileDance(currentRow());
+  } else if (tries >= countOfTries) {
+    youVeryMuchLose();
+  } else {
+    word = "";
+    tries++;
+  }
+};
 
 // FIND ALL LETTERS FOR CURRENT ROW
 const findLettersInRow = () => {
-	let present = [];
-	let correct = [];
-	let wrong = [];
+  let present = [];
+  let correct = [];
+  let wrong = [];
 
-	[...word].forEach((letter, index) => {
-		letter = noAccents(letter)
+  [...word].forEach((letter, index) => {
+    letter = noAccents(letter);
 
-		// letter in correct place
-		if (noAccentSolution.charAt(index) === letter) {
-			correct.push(letter)
-		}
-		// letter in wrong place
-		else if (noAccentSolution.includes(letter)) {
-			present.push(letter)
-		}
-		// wrong number
-		else {
-			wrong.push(letter)
-		}
-	})
+    // letter in correct place
+    if (noAccentSolution.charAt(index) === letter) {
+      correct.push(letter);
+    }
+    // letter in wrong place
+    else if (noAccentSolution.includes(letter)) {
+      present.push(letter);
+    }
+    // wrong number
+    else {
+      wrong.push(letter);
+    }
+  });
 
-	lettersInRow = {
-		present,
-		correct,
-		wrong
-	}
-}
+  lettersInRow = {
+    present,
+    correct,
+    wrong,
+  };
+};
 
 // REMOVE ACCENTS
-function noAccents (str) {
-	return str.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+function noAccents(str) {
+  return str.normalize("NFD").replace(/\p{Diacritic}/gu, "");
 }
 
 // MOBILE
-const keyboard = document.querySelector('.keyboard')
-keyboard.addEventListener('click', (event) => {
-    // clicked on button?
-    if (event.target.nodeName !== 'BUTTON') return
+const keyboard = document.querySelector(".keyboard");
+keyboard.addEventListener("click", (event) => {
+  // clicked on button?
+  if (event.target.nodeName !== "BUTTON") return;
 
-    let character = event.target.id
+  let character = event.target.id;
 
-    /*
+  /*
     if (gameEnded && character === 'again') {
         window.location.reload()
     }
     */
-    if (character === '↵') {
-        submitWord()
-    }
-    else if (character === '←') {
-        removeLetter()
-    }
-    else {
-        addLetter(character)
-    }
-})
+  if (character === "↵") {
+    submitWord();
+  } else if (character === "←") {
+    removeLetter();
+  } else {
+    addLetter(character);
+  }
+});
