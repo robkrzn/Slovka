93ebdcd9aae060755c280cda1026e1c9566a0556
Robert <robert1krizan2@gmail.com>
2022-03-23 11:48:20 +0100

    Zadavanie slova

---
+++ JS/app.js
@@ -0,0 +1,58 @@
+//Data
+let word ='';
+const maxWordLenght = 5;
+
+//keyboard
+document.addEventListener('keydown', (event)=>{
+    if(event.key == 'Enter'){
+        submitWord();
+    }
+    else if(event.key == 'Backspace'){
+        removeLetter();
+    }
+    else{
+        addLetter(event.key);
+    }
+});
+
+//submit
+const submitWord = () => {
+    if( word.length != maxWordLenght) return;
+
+    alert(word)
+}
+
+//add letters
+const addLetter = (character) => {
+    if( word.length >= maxWordLenght) return;
+
+    //allow only latters
+    if(/^\p{L}$/u.test(character)){
+        word = word + character;
+        word = word.toLowerCase();
+
+        let tile = currentTile();
+        tile.innerHTML = character;
+    }
+
+    console.log(word);
+}
+
+const removeLetter = () => {
+    if( word.length <= 0) return;
+
+    let tile = currentTile();
+    tile.innerHTML = '';
+    word = word.slice(0 , -1);
+    console.log(word);
+}
+
+//tile to update
+const currentTile = () => {
+    return currentRow().querySelector(':nth-child('+word.length+')');
+}
+
+//curentRow
+const currentRow = () => {
+    return document.querySelector('.row');
+}
